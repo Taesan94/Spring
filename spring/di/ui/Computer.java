8974b93f8f41b5553566702d197a279ca9799fc1
=taesan <gbgo77@naver.com>
2019-12-29 20:49:53 +0900

    DI 확인

--- spring/di/ui/Computer.java
+++ spring/di/ui/Computer.java
@@ -5,10 +5,10 @@ import spring.di.entity.Mouse;
 public class Computer {
 	private Mouse mouse ;
 	
-	public void connectMouse(Mouse mouse) {
-		this.mouse=mouse;
-	};
 	public void mouseCheck() {
 		mouse.mouseCheck();
 	};
+	public void setMouse(Mouse mouse) {
+		this.mouse=mouse;
+	}
 }
